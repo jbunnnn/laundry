b35e4653003346c67aa2b07313fa5690eff603d6
Rajib Zidan R. <134836561+jbunnnn@users.noreply.github.com>
2025-10-15 05:47:00 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: 'class', // ✅ Tambahin ini buat enable dark mode manual via class
+  darkMode: 'class', 
   theme: {
     extend: {
       colors: {
-        primary: '#2563eb', // Warna utama
+        primary: '#2563eb', 
       },
       screens: {
-        'xs': '475px', // Tambahan breakpoint custom
+        'xs': '475px', 
       },
     },
   },
