990b4b2356615b4c4c6d706959e3242faae32c82
Rajib Zidan R. <134836561+jbunnnn@users.noreply.github.com>
2025-10-15 05:48:31 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,7 +19,7 @@ export default function HomePage() {
           <Services />
         </section>
         
-        <section id="cara-kerja" className="scroll-section"> {/* Pastikan hyphen */}
+        <section id="cara-kerja" className="scroll-section"> {}
           <Steps />
         </section>
         
